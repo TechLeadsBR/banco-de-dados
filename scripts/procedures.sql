da903a4d64d86a4e0df57daf6b6770c2ffdc1c48
carlos-morette <carlos.morette@outlook.com>
2020-09-17 09:31:56 -0300

    Update procedures.sql

--- scripts/procedures.sql
+++ scripts/procedures.sql
@@ -4,3 +4,13 @@ SET @Nome = '%'+@Nome+'%';
 SELECT * FROM VagaEmprego WHERE Titulo LIKE @Nome;
 
 EXEC FiltroVagaDeEmprego '';
+
+CREATE PROCEDURE Buscar 
+@Parametro VARCHAR(200)
+AS
+SET @PARAMETRO = '%'+@Parametro+'%';
+SELECT Nome, NomeVaga, Descricao FROM Vagas INNER JOIN Empresa ON Vagas.IdEmpresa = Empresa.IdEmpresa WHERE NomeVaga LIKE @Parametro
+UNION ALL
+SELECT Nome, NomeVaga, Descricao FROM Vagas INNER JOIN Empresa ON Vagas.IdEmpresa = Empresa.IdEmpresa WHERE Descricao LIKE @Parametro
+UNION ALL
+SELECT Nome, NomeVaga, Descricao FROM Vagas INNER JOIN Empresa ON Vagas.IdEmpresa = Empresa.IdEmpresa WHERE Nome LIKE @Parametro;
