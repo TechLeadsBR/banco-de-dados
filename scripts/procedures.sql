f4859f66298245c9013ccdc9a9195db26cecd4a2
carlos-morette <carlos.morette@outlook.com>
2020-09-18 15:56:42 -0300

    Update procedures.sql

--- scripts/procedures.sql
+++ scripts/procedures.sql
@@ -4,7 +4,7 @@ AS
 SET @PARAMETRO = '%'+@Parametro+'%';
 SELECT * FROM VagaEmprego INNER JOIN Empresa ON VagaEmprego.IdEmpresa = Empresa.IdEmpresa WHERE Titulo LIKE @Parametro
 UNION ALL
-SELECT * FROM VagaEmprego INNER JOIN Empresa ON VagaEmprego.IdEmpresa = Empresa.IdEmpresa WHERE DescricaoEmpresa LIKE @Parametro
+SELECT * FROM VagaEmprego INNER JOIN Empresa ON VagaEmprego.IdEmpresa = Empresa.IdEmpresa WHERE DescricaoVaga LIKE @Parametro
 UNION ALL
 SELECT * FROM VagaEmprego INNER JOIN Empresa ON VagaEmprego.IdEmpresa = Empresa.IdEmpresa WHERE Nivel LIKE @Parametro
 UNION ALL
